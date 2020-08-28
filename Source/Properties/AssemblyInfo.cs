47e23069bd3e1b2739562b7d52ceae78320634e9
hjlee <hjlee@daeyang.co.kr>
2020-08-28 16:08:11 +0900

    v0.2020.828.1607

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2020.0828.1449")]
-[assembly: AssemblyFileVersion("0.2020.0828.1449")]
+[assembly: AssemblyVersion("0.2020.0828.1607")]
+[assembly: AssemblyFileVersion("0.2020.0828.1607")]
 [assembly: NeutralResourcesLanguage("ko-KR")]
