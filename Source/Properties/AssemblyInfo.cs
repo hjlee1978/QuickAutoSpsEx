ef2d36fe24d13d2ff1cd4607e5caba4de28d25de
hjlee <hjlee@daeyang.co.kr>
2020-08-31 13:55:07 +0900

    기타 파일 목록 생성. v0.2020.0831.1351

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2020.0828.1607")]
-[assembly: AssemblyFileVersion("0.2020.0828.1607")]
+[assembly: AssemblyVersion("0.2020.0831.1351")]
+[assembly: AssemblyFileVersion("0.2020.0831.1351")]
 [assembly: NeutralResourcesLanguage("ko-KR")]
